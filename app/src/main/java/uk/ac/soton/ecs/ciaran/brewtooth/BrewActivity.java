f122f30d2cde64cf0beaffa3d25e6bca12fa1e2e
Ciaran Woodward <Ciaran Woodward>
2016-11-12 23:03:02 +0000

    Obtain references to all references in code & basic listener

--- app/src/main/java/uk/ac/soton/ecs/ciaran/brewtooth/BrewActivity.java
+++ app/src/main/java/uk/ac/soton/ecs/ciaran/brewtooth/BrewActivity.java
@@ -2,12 +2,56 @@ package uk.ac.soton.ecs.ciaran.brewtooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class BrewActivity extends AppCompatActivity {
+
+    ProgressBar waterBar;
+    ProgressBar coffeeBar;
+    ProgressBar milkBar;
+
+    LinearLayout waterLayout;
+    LinearLayout coffeeLayout;
+    LinearLayout milkLayout;
+
+    Button brewButton;
+
+    SeekBar strengthBar;
+    SeekBar volumeBar;
+
+    LinearLayout strengthLayout;
+    LinearLayout volumeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brew);
+
+        waterLayout = (LinearLayout) this.findViewById(R.id.layout_water);
+        coffeeLayout = (LinearLayout) this.findViewById(R.id.layout_coffee);
+        milkLayout = (LinearLayout) this.findViewById(R.id.layout_milk);
+        strengthLayout = (LinearLayout) this.findViewById(R.id.layout_strength);
+        volumeLayout = (LinearLayout) this.findViewById(R.id.layout_volume);
+
+        waterBar = (ProgressBar) waterLayout.findViewById(R.id.progressBar_water);
+        coffeeBar = (ProgressBar) coffeeLayout.findViewById(R.id.progressBar_coffee);
+        milkBar = (ProgressBar) milkLayout.findViewById(R.id.progressBar_milk);
+        strengthBar = (SeekBar) strengthLayout.findViewById(R.id.seekBar_strength);
+        volumeBar = (SeekBar) volumeLayout.findViewById(R.id.seekBar_volume);
+
+        brewButton = (Button) this.findViewById(R.id.button_brew);
+
+        brewButton.setOnClickListener(mBrewButtonListener);
     }
+
+    private View.OnClickListener mBrewButtonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            brewButton.setText("Success!");
+        }
+    };
 }
