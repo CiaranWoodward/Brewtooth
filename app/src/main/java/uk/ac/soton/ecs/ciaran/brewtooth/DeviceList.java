beace751950528377385d208965cf01c5998a9c4
Ciaran Woodward <ciaranwoodward@gmail.com>
2016-11-24 00:13:53 +0000

    Added the layout and class for a DeviceListItem
    This includes the potential for status, name, location and capabilities to be shown in the list item.

--- app/src/main/java/uk/ac/soton/ecs/ciaran/brewtooth/DeviceList.java
+++ app/src/main/java/uk/ac/soton/ecs/ciaran/brewtooth/DeviceList.java
@@ -1,7 +1,12 @@
 package uk.ac.soton.ecs.ciaran.brewtooth;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import static uk.ac.soton.ecs.ciaran.brewtooth.R.styleable.View;
 
 public class DeviceList extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class DeviceList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
+        if(savedInstanceState == null){
+           // getSupportFragmentManager().beginTransaction().add(R.id.listview_devices, new PlaceholderFragment()).commit();
+        }
+    }
+
+    //Placeholder
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+        }
     }
 }
